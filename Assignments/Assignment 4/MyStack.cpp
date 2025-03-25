ae930862226db7086458fe04b0e466c30059d258
Kotadama <150510353+Kotadama@users.noreply.github.com>
2025-03-25 11:30:58 -0700

    added files and psuedocode for assignment 4

---
+++ Assignments/Assignment 4/MyStack.cpp
@@ -0,0 +1,47 @@
+#include "MyStack.h" // Include the class header
+#include <climits>
+
+MyStack()
+{
+
+}
+
+MyStack(const MyStack& rhs)
+{
+
+}
+
+MyStack operator=(const MyStack& rhs)
+{
+	//copy contructor
+}
+
+void push(int v) 
+{
+	//add items to the top of the stack
+}
+
+int pop()
+{
+	//remove the item at the top of the stack
+}
+
+int peek()
+{
+	//print the item at the top of the stack
+}
+
+int size()
+{
+	//code for printing out the size of the list/stack
+}
+
+void printStack()
+{
+	//code for printing all elements of the list/Stack
+}
+
+~MyStack()
+{
+	//deconstructor for the stack
+}
